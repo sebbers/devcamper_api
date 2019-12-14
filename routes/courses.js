dac504a39c4aa9a0b418a0622e0a7f0fb7638b06
s <seb.roeder.85@gmail.com>
2019-12-14 09:18:50 -0800

    Update & Delete course

--- routes/courses.js
+++ routes/courses.js
@@ -2,7 +2,9 @@ const express = require('express');
 const {
   getCourses,
   getCourse,
-  addCourse
+  addCourse,
+  updateCourse,
+  deleteCourse
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
@@ -14,7 +16,8 @@ router
 
 router
   .route('/:id')
-  .get(getCourse);
-
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = router;
