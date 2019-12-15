220d19bd47fef12bdd4f3b293dc13eac76f3213f
s <seb.roeder.85@gmail.com>
2019-12-15 15:15:52 -0800

    update user details

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
-
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
 const router = express.Router();
 
 const { protect } = require('../middleware/auth');
@@ -11,7 +10,11 @@ router.post('/login', login);
 
 router.get('/me', protect, getMe);
 
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
+
 router.post('/forgotpassword', forgotPassword);
+
 
 router.put('/resetpassword/:resettoken', resetPassword);
 
