21fee9acef7ad800678406d97470832994a11042
s <seb.roeder.85@gmail.com>
2019-12-15 18:03:39 -0800

    add review for bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -35,9 +35,33 @@ exports.getReview = asyncHandler(async (req, res, next) => {
 
   if(!review){
     return next(
-      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404), 
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404),
     );
   }
+
+  res.status(200).json({
+    success: true,
+    data: review
+  })
+});
+
+
+// @desc    Add review
+// @route   POST /api/v1/bootcamps/:botocampId/reviews
+// @access  Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if(!bootcamp){
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, 404),
+    );
+  }
+
+  const review = await Review.create(req.body);
 
   res.status(200).json({
     success: true,
