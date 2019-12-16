b8b48f58f83a4982a1dfaef8434fd2fa8efad9c6
s <seb.roeder.85@gmail.com>
2019-12-15 16:23:49 -0800

    admin users crud

--- routes/auth.js
+++ routes/auth.js
@@ -5,17 +5,13 @@ const router = express.Router();
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
-
 router.post('/login', login);
 
 router.get('/me', protect, getMe);
-
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
 
 router.post('/forgotpassword', forgotPassword);
-
-
 router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
