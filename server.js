64f023d11323ba609706845b9ecf63b57c6ec343
s <seb.roeder.85@gmail.com>
2019-12-21 20:14:16 -0800

    xss protection & security headers middleware

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -30,7 +32,14 @@ app.use(express.json());
 // Cookie Parser
 app.use(cookieParser());
 
+// Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development'){
